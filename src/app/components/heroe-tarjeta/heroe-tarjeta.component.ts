ccc660e34b61498524ac59b70e5467e5aea82e9d
franciscoLagos18615 <francisco.lagos.s@usach.cl>
2018-10-18 12:53:28 -0300

    @output, hijo hacia padre

--- src/app/components/heroe-tarjeta/heroe-tarjeta.component.ts
+++ src/app/components/heroe-tarjeta/heroe-tarjeta.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Router } from '@angular/router';
 
 
@@ -12,7 +12,9 @@ export class HeroeTarjetaComponent implements OnInit {
   @Input() heroe: any = {};
   @Input() index: number;
 
+  @Output() heroeSeleccionado: EventEmitter<number>;
   constructor(private router: Router) {
+    this.heroeSeleccionado = new EventEmitter();
 
   }
 
@@ -20,8 +22,10 @@ export class HeroeTarjetaComponent implements OnInit {
   }
 
   verHeroe() {
-    console.log(this.index);
-    this.router.navigate (['/heroe', this.index]);
+
+    //console.log(this.index);
+    //this.router.navigate (['/heroe', this.index]);
+    this.heroeSeleccionado.emit( this.index);
 
   }
 
